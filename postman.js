b5921d5a327c05f67a3a8f30cf8eacd1d66b3117
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2023-07-31 17:39:36 +0530

    Add files via upload

--- postman.js
+++ postman.js
@@ -1,8 +1,12 @@
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+const express = require('express')
+const app = express()
+const port = 3000
 
-  app.post('/', (req, res) => {
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
     res.send('Got a POST request')
   })
 
@@ -12,5 +16,9 @@ app.put('/user', (req, res) => {
 
 app.delete('/user', (req, res) => {
     res.send('Got a DELETE request at /user')
+  })
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
   })
   
